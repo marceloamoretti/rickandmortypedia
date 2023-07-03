3b932b1a520d3806ad7bbba9aa20ea3c23d60bd6
Marcelo Amoretti <marceloamoretti@gmail.com>
2023-07-03 11:00:40 -0400

    Feature/theme (#2)
    * feat: add fonts
    
    * chore: update git ignore
    
    * feat: setup theme

---
+++ src/components/ui/components/Text.tsx
@@ -0,0 +1,89 @@
+import React, { memo, useMemo } from 'react';
+import { StyleProp, StyleSheet, Text, TextProps, TextStyle } from 'react-native';
+
+import { theme } from '../theme';
+import { Theme } from '../types';
+
+type FontSize = 'xLarge' | 'large' | 'medium' | 'small' | 'xSmall';
+
+type IProps = {
+  children: string;
+  fontSize: FontSize;
+  color?: keyof Theme['colors'];
+  inverted?: boolean;
+} & TextProps;
+
+const { fontFamily, bodyFontSizes } = theme;
+
+const styles = StyleSheet.create({
+  h1: {
+    fontFamily: fontFamily.medium,
+    fontSize: bodyFontSizes.xLarge,
+    lineHeight: 32,
+  },
+  h2: {
+    fontFamily: fontFamily.medium,
+    fontSize: bodyFontSizes.large,
+    lineHeight: 24,
+  },
+  h3: {
+    fontFamily: fontFamily.medium,
+    fontSize: bodyFontSizes.medium,
+    lineHeight: 20,
+  },
+  h4: {
+    fontFamily: fontFamily.medium,
+    fontSize: bodyFontSizes.small,
+    lineHeight: 18,
+  },
+  h5: {
+    fontFamily: fontFamily.medium,
+    fontSize: bodyFontSizes.xSmall,
+    lineHeight: 16,
+  },
+});
+
+const styleHelper = ({ color, inverted }: Omit<IProps, 'children' | 'fontSize'>) => ({
+  color: color || inverted ? theme.colors.black : theme.colors.white,
+});
+
+export const fontSizeMap: Record<FontSize, StyleProp<TextStyle>> = {
+  xLarge: styles.h1,
+  large: styles.h2,
+  medium: styles.h3,
+  small: styles.h4,
+  xSmall: styles.h5,
+};
+
+const Body = memo<IProps>(({ fontSize, color, inverted, ...textProps }) => {
+  return (
+    <Text
+      style={useMemo(
+        () => [fontSizeMap[fontSize], styleHelper({ color, inverted }), textProps?.style],
+        [color, fontSize, inverted, textProps?.style]
+      )}
+    >
+      {textProps.children}
+    </Text>
+  );
+});
+
+export const TextXLarge = memo<Omit<IProps, 'fontSize'>>(({ ...props }) => (
+  <Body fontSize="xLarge" {...props} />
+));
+
+export const TextLarge = memo<Omit<IProps, 'fontSize'>>(({ ...props }) => (
+  <Body fontSize="large" {...props} />
+));
+
+export const TextMedium = memo<Omit<IProps, 'fontSize'>>(({ ...props }) => (
+  <Body fontSize="medium" {...props} />
+));
+
+export const TextSmall = memo<Omit<IProps, 'fontSize'>>(({ ...props }) => (
+  <Body fontSize="small" {...props} />
+));
+
+export const TextXSmall = memo<Omit<IProps, 'fontSize'>>(({ ...props }) => (
+  <Body fontSize="xSmall" {...props} />
+));
---
+++ src/components/ui/theme.tsx
@@ -0,0 +1,107 @@
+import { Theme } from './types';
+
+const Colors = {
+  primary: '#6842FF',
+  primary100: '#8668FF',
+  primary200: '#A48EFF',
+  primary300: '#C3B3FF',
+  primary400: '#F0ECFF',
+
+  secondary: '#FFD300',
+  secondary100: '#FFDC33',
+  secondary200: '#FFE566',
+  secondary300: '#FFED99',
+  secondary400: '#FFFBE6',
+
+  /* Alert & Status */
+  success: '#4ADE80',
+  info: '#246BFD',
+  warning: '#FACC15',
+  error: '#F75555',
+  disabled: '#D8D8D8',
+  disabledButton: '#6B5BAB',
+
+  /* Greyscale */
+  grey900: '#212121',
+  grey800: '#424242',
+  grey700: '#616161',
+  grey600: '#757575',
+  grey500: '#9E9E9E',
+  grey400: '#BDBDBD',
+  grey300: '#E0E0E0',
+  grey200: '#EEE',
+  grey100: '#F5F5F5',
+  grey50: '#FAFAFA',
+
+  /* Gradients */
+  gradientPurple: ['#6842FF', '#896BFF'],
+  gradientGreen: ['#1AB65C', '#39E180'],
+  gradientBlue: ['#246BFD', '#6F9EFF'],
+  gradientYellow: ['#FACC15', '#FFE580'],
+  gradientRed: ['#FF5A5F', '#FF9094'],
+
+  /* Dark Colors */
+  dark1: '#181A20',
+  dark2: '#1F222A',
+  dark3: '#35383F',
+
+  /* Others */
+  white: '#FFF',
+  black: '#000',
+  red: '#F54336',
+  pink: '#EA1E61',
+  purple: '#9D28AC',
+  deepPurple: '#673AB3',
+  indigo: '#3F51B2',
+  blue: '#1A96F0',
+  lightBlue: '#00A9F1',
+  cyan: '#00BCD3',
+  teal: '#009689',
+  green: '#4AAF57',
+  lightGreen: '#8BC255',
+  lime: '#CDDC4C',
+  yellow: '#FFEB4F',
+  amber: '#FFC02D',
+  orange: '#FF981F',
+  deepOrange: '#FF5726',
+  brown: '#7A5548',
+  blueGrey: '#607D8A',
+
+  /* Background */
+  bgGreen: '#F6FFFA',
+  bgBlue: '#F6FAFD',
+  bgRed: '#FFF5F5',
+  bgYellow: '#FFFEE0',
+  bgPurple: '#FCF4FF',
+};
+
+const FontFamily = {
+  bold: 'Urbanist-Bold',
+  semibold: 'Urbanist-SemiBold',
+  medium: 'Urbanist-Medium',
+  regular: 'Urbanist-Regular',
+};
+
+const BodyFontSizes = {
+  xLarge: 18,
+  large: 16,
+  medium: 14,
+  small: 12,
+  xSmall: 10,
+};
+
+const HeadingFontSizes = {
+  h1: 48,
+  h2: 40,
+  h3: 32,
+  h4: 24,
+  h5: 20,
+  h6: 18,
+};
+
+export const theme: Theme = {
+  colors: Colors,
+  fontFamily: FontFamily,
+  bodyFontSizes: BodyFontSizes,
+  headingFontSizes: HeadingFontSizes,
+};
