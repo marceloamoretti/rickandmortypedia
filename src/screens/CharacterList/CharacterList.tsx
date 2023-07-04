d46816c1290d0a64cb4cf0a22d8a06a718d8f84c
Marcelo Amoretti <marceloamoretti@gmail.com>
2023-07-03 21:16:21 -0400

    Feature/splash screen (#14)
    * feat: add splash screen
    
    * rename app

--- src/screens/CharacterList/CharacterList.tsx
+++ src/screens/CharacterList/CharacterList.tsx
@@ -1,4 +1,5 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 
 import useCharacters from '~components/characters/api/hooks/useCharacters';
 import List from '~components/characters/components/List/List';
@@ -6,6 +7,14 @@ import List from '~components/characters/components/List/List';
 const CharacterList = memo(() => {
   const [searchInput, setSearchInput] = useState<string | undefined>(undefined);
   const list = useCharacters({ name: searchInput });
+
+  const handleSplash = async (): Promise<void> => {
+    await RNBootSplash.hide({ fade: true, duration: 500 });
+  };
+
+  useEffect(() => {
+    void handleSplash();
+  }, []);
 
   return <List list={list} searchInput={searchInput} setSearchInput={setSearchInput} />;
 });
